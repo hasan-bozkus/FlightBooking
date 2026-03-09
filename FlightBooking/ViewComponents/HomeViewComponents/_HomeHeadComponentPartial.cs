8689f18e3b097c0841c7ea6f0bb42a80d1177e7d
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2026-03-09 22:12:39 +0300

    AI Destekli Uçak Bileti Rezervasyon Sistemi
    Bölüm 1: Projeye Giriş ve Mevcut Sistem Analizi
    Bölüm 2: Projenin Oluşturulması ve Tema Entegrasyonu
    Bölüm 3: View Component ile Layout Yapısının Oluşturulması

---
+++ FlightBooking/ViewComponents/HomeViewComponents/_HomeHeadComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightBooking.ViewComponents.HomeViewComponents
+{
+    public class _HomeHeadComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View(); 
+        }
+    }
+}
