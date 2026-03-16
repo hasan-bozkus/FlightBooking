1a98664e1c7237c9766e0ef5376fc62ffd876343
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2026-03-16 23:43:35 +0300

    AI Destekli Uçak Bileti Rezervasyon Sistemi
    Bölüm 5: Ana Sayfa Slider Alanının Oluşturulması

---
+++ FlightBooking/ViewComponents/HomeViewComponents/_HomeSliderComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightBooking.ViewComponents.HomeViewComponents
+{
+    public class _HomeSliderComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
