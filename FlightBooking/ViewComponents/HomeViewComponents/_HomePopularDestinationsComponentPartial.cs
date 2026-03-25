e14c09b68d7baa4c564845fa82c20775d0e480b1
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2026-03-25 21:35:33 +0300

    AI Destekli Uçak Bileti Rezervasyon Sistemi
    Bölüm 9: Popüler Uçuşlar ve Destinasyon Kartları Bölümü

---
+++ FlightBooking/ViewComponents/HomeViewComponents/_HomePopularDestinationsComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightBooking.ViewComponents.HomeViewComponents
+{
+    public class _HomePopularDestinationsComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
