7d6694626f4325d31082c61e39e365a0f33c7909
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2026-04-20 21:05:35 +0300

    AI Destekli Uçak Bileti Rezervasyon Sistemi
    Bölüm 25: ChatGPT 50 Uçuş Verisi Oluşturma (Json)
    Bölüm 26: Claude AI ile Uçuş Listesini Modernleştirme
    Bölüm 27: Flight & Passenger Entity ve DTO Tasarımı

---
+++ FlightBooking/Dtos/BookingDtos/CreateBookingDto.cs
@@ -0,0 +1,18 @@
+﻿using FlightBooking.Dtos.PassengersDtos;
+
+namespace FlightBooking.Dtos.BookingDtos
+{
+    public class CreateBookingDto
+    {
+        public string FlightId { get; set; }
+        public decimal TotalPrice { get; set; }
+        public DateTime BookingDate { get; set; }
+        public string Status { get; set; } // Pending, Confirmed, Cancelled
+
+        public string ContactName { get; set; }
+        public string ContactEmail { get; set; }
+        public string ContactPhone { get; set; }
+
+        public List<CreatePassengerDto> Passengers { get; set; }
+    }
+}
---
+++ FlightBooking/Entities/Passenger.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace FlightBooking.Entities
+{
+    public class Passenger
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Gender { get; set; }
+        public string PassengerType { get; set; }
+    }
+}
