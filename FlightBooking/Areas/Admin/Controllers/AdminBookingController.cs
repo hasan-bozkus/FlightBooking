85c297c84252f77269b3cfb103ebd525049afe86
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2026-04-23 22:21:38 +0300

    AI Destekli Uçak Bileti Rezervasyon Sistemi
    Bölüm 30:Seçilen Uçuşu Rezervasyon Sayfasına Taşıma
    Bölüm 31: Booking Service (Interface & Service Katmanı)

--- FlightBooking/Areas/Admin/Controllers/AdminBookingController.cs
+++ FlightBooking/Areas/Admin/Controllers/AdminBookingController.cs
@@ -1,17 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FlightBooking.Services.FilghtServices;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FlightBooking.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class AdminBookingController : Controller
     {
+        private readonly IFlightService _flightService;
+
+        public AdminBookingController(IFlightService flightService)
+        {
+            _flightService = flightService;
+        }
+
         public IActionResult BookingList()
         {
             return View();
         }
 
-        public IActionResult CreateBooking()
+        [HttpGet]
+        public async Task<IActionResult> CreateBooking(string id)
         {
+            var value = await _flightService.GetFlightByIdDtoAsync(id);
+            ViewBag.FlightNumber = value.FlightNumber;
+            ViewBag.DepartureAirportCode = value.DepartureAirportCode;
+            ViewBag.DepartureAirportName = value.DepartureAirportName;
+            ViewBag.ArrivalAirportCode = value.ArrivalAirportCode;
+            ViewBag.ArrivalAirportName = value.ArrivalAirportName;
+            ViewBag.DepartureTime = value.DepartureTime;
+            ViewBag.ArrivalTime = value.ArrivalTime;
+            ViewBag.AirlineCode = value.AirlineCode;
+
             return View();
         }
     }
---
+++ FlightBooking/Services/BookingServices/IBookingService.cs
@@ -0,0 +1,13 @@
+﻿using FlightBooking.Dtos.BookingDtos;
+
+namespace FlightBooking.Services.BookingServices
+{
+    public interface IBookingService
+    {
+        //Task<List<ResultBookingDto>> GetAllFilghtAsync();
+        //Task<GetBookingByIdDto> GetBookingByIdDtoAsync(string id);
+        Task CreateBookingAsync(CreateBookingDto createBookingDto);
+        //Task DeleteBookingAsync(string id);
+        //Task UpdateBookingAsync(UpdateBookingDto updateBookingDto);
+    }
+}
